fccc5661cdc7723828710ebdc0f00262fbed5a20
H0rn0chse <h0rn0chse@gmx.de>
2022-05-16 11:03:33 +0200

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "@vue/airbnb",
+    ],
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    rules: {
+        indent: ["error", 4, { SwitchCase: 1 }],
+        quotes: ["error", "double"],
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "space-before-function-paren": ["error", "always"],
+        "comma-dangle": "off"
+    },
+};
