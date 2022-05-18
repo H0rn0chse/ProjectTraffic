1b5d8e2e63ab2d947ba492d435d266fe5d96f158
H0rn0chse <h0rn0chse@gmx.de>
2022-05-18 14:00:44 +0200

    Added Filter and Grouping group

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,33 +4,84 @@
             <h1 class="display-2 font-weight-bold mb-3">
                 Project Traffic Summary
             </h1>
-            <div class="d-flex flex-row w-100">
+            <div class="d-flex flex-row w-100 mb-2">
                 <v-btn
-                    title="All (Clear all preferences)"
+                    class="me-2"
+                    title="Reset Filter and Grouping"
+                    @click="resetFilter"
                 >
-                    All
+                    Reset Filter and Grouping
+                </v-btn>
+                <v-btn
+                    class="me-2"
+                    title="Redraw Chart"
+                    @click="redrawChart"
+                >
+                    Redraw Chart
                 </v-btn>
             </div>
-            <div class="d-flex flex-row w-100">
-                <v-radio-group
-                    v-model="groupBy"
-                    inline
-                    label="GroupBy"
-                >
-                    <v-radio
-                        label="None"
-                        value="none"
-                    ></v-radio>
-                    <v-radio
-                        label="DataProvider"
-                        value="provider"
-                    ></v-radio>
-                    <v-radio
-                        label="Project"
-                        value="project"
-                    ></v-radio>
-                </v-radio-group>
-            </div>
+            <v-expansion-panels class="mb-10">
+                <v-expansion-panel>
+                    <v-expansion-panel-title>
+                        Filter And Group Data
+                    </v-expansion-panel-title>
+                    <v-expansion-panel-text>
+                        <div class="d-flex flex-row w-100">
+                            <v-radio-group
+                                v-model="groupBy"
+                                inline
+                                label="GroupBy"
+                            >
+                                <v-radio
+                                    label="None"
+                                    value="none"
+                                ></v-radio>
+                                <v-radio
+                                    label="DataProvider"
+                                    value="provider"
+                                ></v-radio>
+                                <v-radio
+                                    label="Project"
+                                    value="project"
+                                ></v-radio>
+                            </v-radio-group>
+                        </div>
+                        <div class="d-flex flex-row w-100">
+                            <v-select
+                                v-model="filteredProviderTypes"
+                                :items="providerTypes"
+                                label="Filter by ProviderType"
+                                multiple
+                                chips
+                                persistent-hint
+                                dense
+                            ></v-select>
+                        </div>
+                        <div class="d-flex flex-row w-100">
+                            <v-select
+                                v-model="filteredProviders"
+                                :items="providers"
+                                label="Filter by DataProvider"
+                                multiple
+                                chips
+                                persistent-hint
+                                dense
+                            ></v-select>
+                        </div>
+                        <div class="d-flex flex-row w-100">
+                            <v-select
+                                v-model="filteredProjects"
+                                :items="projects"
+                                label="Filter by Project"
+                                multiple
+                                chips
+                                persistent-hint
+                                dense
+                            ></v-select>
+                        </div>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+            </v-expansion-panels>
             <bar
                 :chart-options="chartOptions"
                 :chart-data="chartData"
@@ -46,7 +97,7 @@
 <script>
 import { defineComponent } from "vue";
 import { Bar } from "vue-chartjs";
-import { mapGetters } from "vuex";
+import { mapGetters, mapState } from "vuex";
 import { DATASET_TYPES } from "../store/data";
 import { lighten } from "../utils/colors";
 
@@ -54,6 +105,9 @@ export default defineComponent({
     name: "HomeView",
     data: () => ({
         groupBy: "none",
+        filteredProviderTypes: [],
+        filteredProviders: [],
+        filteredProjects: [],
         chartOptions: {
             responsive: false,
             plugins: {
@@ -87,7 +141,30 @@ export default defineComponent({
     computed: {
         ...mapGetters("data", {
             chartData: "filteredAndGroupedData",
-        })
+        }),
+        ...mapState("provider", {
+            providers: (state) => {
+                return state.dataProviders.map((provider) => {
+                    return { value: provider.id, title: provider.name };
+                });
+            },
+            providerTypes: "supportedDataProviderTypes",
+        }),
+        ...mapState("project", {
+            projects: (state) => {
+                return state.projects.map((project) => {
+                    return { value: project.id, title: project.name };
+                });
+            },
+        }),
+    },
+    methods: {
+        resetFilter () {
+            alert("todo: resetFilter");
+        },
+        redrawChart () {
+            alert("todo: redrawChart");
+        }
     },
     components: { Bar },
 });
