b6455283b2dbabdb9764312d3f0491d2a81bc05a
H0rn0chse <h0rn0chse@gmx.de>
2022-05-20 11:00:09 +0200

    Reverted latest changes

--- server/adapters/umami.js
+++ server/adapters/umami.js
@@ -110,7 +110,7 @@ class _Umami {
         const result = data
             .map((referrer) => {
                 return {
-                    referrer: referrer.x.toString()
+                    referrer: referrer.x
                         .replaceAll("https://", "")
                         .replaceAll("http://", "")
                         .replaceAll("www.", ""),
